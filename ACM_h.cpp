46c94cf03e842d8d2156d02b8d40779c59719e90
iCat527 <iCat527@163.com>
2015-04-17 16:38:55 +0800

    header

---
+++ ACM_h.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <algorithm>
+#define INT_MAX 0x7fffffff
+#define MAX(a,b) ((a)>(b)?(a):(b))
+#define MIN(a,b) ((a)<(b)?(a):(b))
+using namespace std;
+
+typedef long long LL;
+
+int main()
+{
+    
+}
