655533feb5896aaffdf113c9127433c4c53248be
iCat527 <iCat527@163.com>
2015-05-01 09:48:09 +0800

    Update ACM_h.cpp

--- ACM_h.cpp
+++ ACM_h.cpp
@@ -4,10 +4,10 @@
 #include <cmath>
 #include <algorithm>
 #define INT_MAX 0x7fffffff
+#define INT_MIN 0x80000000
 #define MAX(a,b) ((a)>(b)?(a):(b))
 #define MIN(a,b) ((a)<(b)?(a):(b))
 using namespace std;
-
 typedef long long LL;
 
 int main()
